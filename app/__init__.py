fb410fc6c5148a2be307a8d0eda65c5ebc7c4951
pavelvasilenko <vas.pavel67.work@gmail.com>
2021-05-19 13:02:46 +0300

    Структура проекта

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+login = LoginManager()
+login.login_view = 'users.login'
+login.login_message_category = 'info'
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+
+    with app.app_context():
+        pass
